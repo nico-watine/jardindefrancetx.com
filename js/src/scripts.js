b956635d9cea3aa841183b86dc7a0d3555d3720d
Nico Watine <contact@studioncreations.com>
2015-08-19 13:54:53 -0500

    Added import documentation to scripts.js
    Changed import of slick.js to slick.min.js

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -1,8 +1,17 @@
+// PREPENDS ::
+	// ASSETS/BOWER_COMPONENTS/SLICK-CAROUSEL/SLICK/SLICK.MIN.JS
+
+// APPENDS ::
+	// JS/SRC/NAV/RESPONSIVE-NAV.JS
+	// JS/SRC/NAV/FASTCLICK.JS
+	// JS/SRC/NAV/SCROLL.JS
+	// JS/SRC/NAV/FIXED-RESPONSIVE-NAV.JS
+
 $(document).ready(function(){
 	$('.section-bg').slick({
 	  	fade: true,
 	    draggable: false,
-	    swipe: false,
-	    lazyLoad: 'progressive'
+	    swipe: false//,
+	    // lazyLoad: 'progressive'
 	});
 });
