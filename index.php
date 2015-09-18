390a1a8f10474a709b5b0e3ce975fc74c4337610
Nico Watine <contact@studioncreations.com>
2015-09-18 16:29:17 +0200

    Added new <script> to local jQuery reference

--- index.php
+++ index.php
@@ -181,6 +181,7 @@
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/vendor/jquery/1-11-3/jquery-min.js"><\/script>')</script>
 <script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
 </body>
 </html>
