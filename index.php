bfac76dfed488786cab15d1c44f37a077bf1a9f9
Nico Watine <contact@studioncreations.com>
2015-11-01 13:23:53 -0600

    Edited <head>

--- index.php
+++ index.php
@@ -5,21 +5,21 @@
 <html class="no-js" lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
 	<title>Jardin de France | Houston's French Antiques and Plants for the Home &amp; Patio</title>
-
 	<meta name="description" content="Bringing to Houston the beauty of original French Country antiques &amp; quaint garden design">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="keywords" content="Antique Show Houston, French Antique Store Houston, European Antique Furniture, French Antiques Online, French Settees, Architectural Elements">
-	<link type="text/plain" rel="author" href="/humans.txt">
 
-	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="author" href="/humans.txt">
 	
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
+
 	<!--[if lt IE 9]>
-		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>" />
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
+		<script src="<?php autoVer('/js/ie-min.js'); ?>"></script>
 	<![endif]-->
+
 </head>
 <body id="index">
 
