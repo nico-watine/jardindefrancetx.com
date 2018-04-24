9bffd6371228a91ed83bcb79ccd4d8791ef8a082
Nico Watine <nico@studioncreations.com>
2018-04-24 08:24:46 -0500

    Grid successfuly installed, merge & close

--- dev.php
+++ dev.php
@@ -20,7 +20,7 @@
 <div class="row" style="border: 4px solid blue;">
 	<br>
 	<br>
-	<div class="small-8 small-push-1 medium-7 medium-push-5 large-8 columns large-push-2 end" style="border: 4px solid red;">
+	<div class="small-8 small-push-1 medium-7 medium-push-5 large-8 columns large-push-3 end" style="border: 4px solid red;">
 		<h3 style="font-family: Arial;font-size: 28px;color: #0A0A0A;text-align: center;line-height: 1.4; font-weight: 400;">There is an even larger selection of indoor plants available at our online plant shop.</h3>
 	</div>
 </div>
