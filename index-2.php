b3f7c4ffd308c4bf17012fa5230b4d852a05a65f
Nico Watine <nico@studioncreations.com>
2018-04-24 12:44:55 -0500

    Null new video code to focus on prose markup

--- index-2.php
+++ index-2.php
@@ -53,10 +53,10 @@
 <body id="index-2">
 
 <section class="splash">
-	<video autoplay loop preload="auto" poster="/img/index/splash/poster.jpg" id="backgroundVideo">
+<!-- 	<video autoplay loop preload="auto" poster="/img/index/splash/poster.jpg" id="backgroundVideo">
 		<source src="/img/index/splash/splash.webm" type="video/webm">
 		<source src="/img/index/splash/splash.mp4" type="video/mp4">
-	</video>
+	</video> -->
 </section>
 <!-- <section class="splash bgvid-container" id="welcome">
 	<div class="verbiage">
