c2ec363e6d983731c7e5bc062e005a50c0781115
Nico Watine <nico@studioncreations.com>
2018-04-24 12:28:27 -0500

    Duplicate index into 2nd copy

---
+++ index-2.php
@@ -0,0 +1,233 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#" class="no-js" lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Jardin de France | Houston's French Antiques and Plants for the Home &amp; Patio</title>
+	<meta name="description" content="Bringing to Houston the beauty of original French Country antiques &amp; quaint garden design">
+	<meta name="keywords" content="Antique Show Houston, French Antique Store Houston, European Antique Furniture, French Antiques Online, French Settees, Architectural Elements">
+	<link href="http://jardindefrancetx.com" rel="canonical">
+	<link href="/humans.txt" rel="author">
+	
+	<meta property="og:locale" content="en_US">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Jardin de France - featured in MILIEU-MAG.com">
+	<meta property="og:description" content="We are featured in the Spring 2017 publication of MILIEU! The full article is available on our website.">
+	<meta property="og:url" content="http://jardindefrancetx.com">
+	<meta property="og:site_name" content="Jardin de France">
+	<meta property="og:image" content="http://jardindefrancetx.com/img/social-media/milieu/og-image.png">
+
+	<!-- <link href="<?php autoVer('/css/opt/style.css'); ?>" rel="stylesheet"> -->
+	<link href="<?php autoVer('/css/style.css'); ?>" rel="stylesheet">
+
+	<script type="application/ld+json">
+	{
+	  "@context": "http://schema.org",
+	  "@type": "LocalBusiness",
+	  "name": "Jardin de France",
+	  "logo": "http://jardindefrancetx.com/img/assets/logo/schema/jardin-de-france-logo.png",
+	  "description": "Bringing to Houston the beauty of original French Country antiques &amp; quaint garden design",
+	  "url" : "http://jardindefrancetx.com",
+	  "telephone": "832-651-3136",
+	  "address": {
+	    "@type": "PostalAddress",
+	    "addressLocality": "Houston",
+	    "addressRegion": "TX",
+	    "postalCode": "77007",
+	    "streetAddress": "4819 Blossom St"
+	  },
+	  "sameAs" : [
+	    "https://www.google.com/+JardindeFranceHouston",
+	    "https://www.facebook.com/jardindefrancetx/",
+	    "https://www.pinterest.com/lebasi68/jardin-de-france/",
+	    "https://www.instagram.com/gloria_jardindefrance/"
+	  ]
+	}
+	</script>
+</head>
+<body id="index">
+
+<header>
+	<div class="outer-container">
+		<div class="row">
+			<div class="logo">
+				<a href="#welcome" data-scroll><img src="/img/assets/logo/logo.png" class="pot" alt="Jardin de France Logo Icon"></a>
+				<a href="#welcome" data-scroll><h1 class="title">JdF</h1></a>
+			</div>
+			<nav class="nav-collapse">
+				<ul>
+					<li class="menu-item active" id="first"><a href="#welcome" data-scroll>Welcome</a></li>
+					<li class="menu-item"><a href="#gardens" data-scroll>The Gardens</a></li>
+					<li class="menu-item"><a href="#conservatory" data-scroll>The Conservatory</a></li>
+					<li class="menu-item"><a href="#interior_gallery" data-scroll>The Interior Gallery</a></li>
+					<li class="menu-item"><a href="#about" data-scroll>Our Story</a></li>
+					<li class="menu-item"><a href="#location" data-scroll>Location</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</header>
+
+<section class="splash bgvid-container" id="welcome">
+	<div class="featureFocus-milieu" id="default">
+		<p>We will be featured in the Spring 2018 issue of MILIEU Mag!</p>
+		<a href="http://milieu-mag.com/Story/MY_HOUSTON/279"><img srcset="/img/index/milieu-banner/logo_2x.png 2x" src="/img/index/milieu-banner/logo.png" alt="Milieu Mag Logo"></a>
+		<a href="/img/social-media/milieu/spring-2017-my-houston.pdf" class="buttonGhost">Here is our Spring 2017 Milieu ad</a>
+	</div>
+	<div class="verbiage">
+		<h1 class="title">Jardin de France</h1>
+		<h2 class="slogan">French Antiques and Plants for the <span class="inline-block">Home &amp; Patio</span></h2>
+		<h3 class="tag">Bringing to Houston the beauty of original French Country antiques &amp; quaint <span class="inline-block">garden design</span></h3>
+		<a href="#gardens" class="button" data-scroll>Come Visit Us!</a>
+	</div>
+
+	<video autoplay loop preload="auto" poster="/img/index/splash/poster.jpg" id="bgvid">
+		<source src="/img/index/splash/splash.webm" type="video/webm">
+		<source src="/img/index/splash/splash.mp4" type="video/mp4">
+	</video>
+</section>
+
+<section class="featureFocus-milieu" id="small">
+	<p>We are featured in the Spring 2017 issue of <span class="inline-block">MILIEU Mag!</span></p>
+	<a href="http://milieu-mag.com/Story/MY_HOUSTON/279"><img srcset="/img/index/milieu-banner/logo_2x.png 2x" src="/img/index/milieu-banner/logo.png" alt="Milieu Mag Logo"></a>
+	<a href="/img/social-media/milieu/spring-2017-my-houston.pdf" class="buttonGhost">Read the full article</a>
+</section>
+
+<section class="gardens" id="gardens">
+		<div class="section-content">
+			<div class="outer-container">
+				<div class="row padding">
+					<h1 class="title">The Gardens</h1>
+					<h3>Find unique patio pieces and design inspiration for you own backyard. We can help you create a private retreat in the comfort of your home.</h3>
+				</div>
+			</div>
+		</div>
+		<div class="section-bg slick-slider">
+			<div class="slide-1">
+				<img src="/img/index/gardens/default/garden-1.jpg" alt="Jardin de France Fountain Garden">
+			</div>
+			<div class="slide-2">
+				<img src="/img/index/gardens/default/garden-2.jpg" alt="Jardin de France Patio Garden">
+			</div>
+			<div class="slide-3">
+				<img src="/img/index/gardens/default/garden-3.jpg" alt="Jardin de France Entrance Path">
+			</div>
+		</div>
+</section>
+
+<section class="conservatory" id="conservatory">
+		<div class="section-content">
+			<div class="outer-container">
+				<div class="row padding">
+					<h1 class="title">The Conservatory</h1>
+					<h3>We have a variety of beautiful indoor plants and lovely succulents for sale in The Conservatory</h3>
+				</div>
+			</div>
+		</div>
+		<div class="section-bg slick-slider">
+			<div class="slide-1">
+				<img src="/img/index/conservatory/default/conservatory-1.jpg" alt="Jardin de France Conservatory Building">
+			</div>
+			<div class="slide-2">
+				<img src="/img/index/conservatory/default/conservatory-2.jpg" alt="Pottery and Plants in the Conservatory">
+			</div>
+			<div class="slide-3">
+				<img src="/img/index/conservatory/default/conservatory-3.jpg" alt="A stone fountain filled with plants">
+			</div>
+		</div>
+</section>
+
+<section class="hip-banner linen-bg">
+	<div class="outer-container">
+		<div class="row padding">
+			<h3 class="no-shadow">There is an even larger selection of indoor plants available at our <span>online plant shop</span></h3>
+			<a href="http://www.houstoninteriorplants.com" target="_blank" class="button">www.<span>H</span>ouston<span>I</span>nterior<span>P</span>lants.com</a>
+		</div>
+	</div>
+</section>
+
+<section class="interior_gallery" id="interior_gallery">
+		<div class="section-content">
+			<div class="outer-container">
+				<div class="row padding">
+					<h1 class="title">The Interior Gallery</h1>
+					<h3>In addition to our plants and garden items, we also have fine French Country antiques</h3>
+				</div>
+			</div>
+		</div>
+		<div class="section-bg slick-slider">
+			<div class="slide-1">
+				<img src="/img/index/interior_gallery/default/interior_gallery.jpg" alt="Jardin de France Interior Gallery">
+			</div>
+		</div>
+</section>
+
+<section class="about linen-bg" id="about">
+	<div class="outer-container">
+		<div class="row padding" id="story">
+			<h1 class="title no-shadow">Our Story</h1>
+			<h3 class="no-shadow">Two brothers born in the small Picard village of Montrelet, Nicolas and Patrice Watine have partnered to select and bring to you a taste of their antique furnishing heritage.</h3>
+		</div>
+		<div class="row" id="pics">
+			<div class="profile-pic" id="patrice">
+				<img src="/img/index/about/patrice.png" alt="Patrice Watine">
+			</div>
+			<div class="profile-pic" id="nicolas">
+				<img src="/img/index/about/nicolas.png" alt="Nicolas Watine">
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="location" id="location">
+	<div class="outer-container">
+		<div class="row">
+			<div class="section-content">
+				<div class="location-verbiage">
+					<h1 class="title">Location</h1>
+					<h4>We are conveniently located 2 blocks south of Washington Ave &amp; Durham Dr</h4>
+					<h3>Look for the French flag!</h3>
+					<p class="large">Tuesday - Saturday <br> 10AM - 5PM</p>
+					<p class="large"><a id="phone" href="tel://832-651-3136" class="phone-number">832-651-3136</a></p>
+					<p class="large"><a href="mailto:shop@jdftx.com?Subject=Bonjour!" target="_top">Shop@JDFTX.com</a></p>
+					<p class="large"><a id="map-link" href="https://www.google.com/maps/place/4819+Blossom+St,+Houston,+TX+77007/@29.767306,-95.410378,17z/data=!3m1!4b1!4m2!3m1!1s0x8640c0b46e30ca21:0x9fb821a40a5d7107" target="_blank">4819 Blossom St. <br>Houston, TX <br>77007</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- <div class="map-banner"></div> -->
+
+<footer class="linen-bg">
+	<div class="outer-container">
+		<div class="row">
+			<ul class="share-buttons">
+				<li><a href="https://www.facebook.com/jardindefrancetx" target="_blank"><img class="normal" src="/img/assets/social/facebook.png" alt="Facebook"><img class="hover" src="/img/assets/social/facebook-hover.png" alt="Facebook"></a></li>
+				<li><a href="https://instagram.com/gloria_jardindefrance/" target="_blank"><img class="normal" src="/img/assets/social/instagram.png" alt="Instagram"><img class="hover" src="/img/assets/social/instagram-hover.png" alt="Instagram"></a></li>
+				<li><a href="https://www.pinterest.com/patglo68/" target="_blank"><img class="normal" src="/img/assets/social/pinterest.png" alt="Pinterest"><img class="hover" src="/img/assets/social/pinterest-hover.png" alt="Pinterest"></a></li>
+			</ul>
+		</div>
+		<div class="row">
+			<h5 id="credit"><a class="no-shadow" href="http://studioncreations.com" target="_blank">A <span>Studio N</span> Creation</a></h5>
+		</div>
+	</div>
+</footer>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/vendor/jquery/1-11-3/jquery-min.js"><\/script>')</script>
+<script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
+
+<script>
+    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+    ga('create','UA-85408517-2','auto');ga('send','pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+
+</body>
+</html>
