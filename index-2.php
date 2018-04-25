632e5a09027b6410f61e35e8ba8e6d03199480c3
Nico Watine <nico@studioncreations.com>
2018-04-25 17:34:11 -0500

    Correctly position the 2 profile images

--- index-2.php
+++ index-2.php
@@ -113,13 +113,9 @@
 				<h2 class="h1 cursive">Our Story</h2>
 				<p class="h4">Two brothers born in the small Picard village of Montrelet, Nicolas and Patrice Watine have partnered to select and bring to you a taste of their antique furnishing patrimoine.</p>
 			</div>
-			<div class="bioPhoto">
-				<div class="padding-X">
-					<img src="/img/index-2/about/nicolas-watine.png" alt="">
-				</div>
-				<div class="padding-X">
-					<img src="/img/index-2/about/patrice-watine.png" alt="">
-				</div>
+			<div class="bioPhoto text-center">
+				<img class="margin-X" src="/img/index-2/about/nicolas-watine.png" alt="">
+				<img class="margin-X" src="/img/index-2/about/patrice-watine.png" alt="">
 			</div>
 		</div>
 	</div>
