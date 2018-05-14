c47811fc3452b019c14495048640d5b6ec5898e6
Nico Watine <nico@studioncreations.com>
2018-05-14 15:33:03 -0500

    Reinstate final image into large, no longer xlarge

--- index.php
+++ index.php
@@ -145,9 +145,9 @@
 			<div class="small-10 small-pull-1 medium-pull-0 medium-4 large-3 columns">
 				<img src="/img/index-2/interiorGallery/product/antique-home-pottery.jpg" alt="Antique home pottery">
 			</div>
-			<!-- <div class="show-for-xlarge large-3 columns">
+			<div class="show-for-large large-3 columns">
 				<img src="/img/index-2/interiorGallery/product/vintage-french-books-lamp.jpg" alt="Vintage French books by lamp">
-			</div> -->
+			</div>
 		</div>
 	</div>
 	<br>
