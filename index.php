ef54de33f58c13012cce8e9cca26d591e10fff9f
Nico Watine <nico@studioncreations.com>
2018-05-14 14:49:07 -0500

    Copy/paste the markup for the product row

--- index.php
+++ index.php
@@ -134,6 +134,21 @@
 			</div>
 		</div>
 	</div>
+	<br>
+	<div class="row collapse" style="max-width: 1600px;">
+		<div class="medium-4 large-3 columns">
+			<img style="border-right: 3px solid #fff;" src="/img/index-2/interiorGallery/product/antique-french-knives-set.jpg" alt="">
+		</div>
+		<div class="medium-4 large-3 columns">
+			<img style="border-right: 3px solid #fff;" src="/img/index-2/interiorGallery/product/french-vintage-home-goods.jpg" alt="">
+		</div>
+		<div class="medium-4 large-3 columns">
+			<img style="border-right: 3px solid #fff;" src="/img/index-2/interiorGallery/product/antique-home-pottery.jpg" alt="">
+		</div>
+		<div class="show-for-large large-3 columns">
+			<img style="border-right: 3px solid transparent;" src="/img/index-2/interiorGallery/product/vintage-french-books-lamp.jpg" alt="">
+		</div>
+	</div>
 </section>
 
 <section class="about" style="padding: 40px 0;" id="about">
