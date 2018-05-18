b90c5934fa43301aee98276d7168821156bfdfa8
Nico Watine <nico@studioncreations.com>
2018-05-17 21:27:07 -0500

    Working small up

--- gardens.php
+++ gardens.php
@@ -18,11 +18,20 @@
 <body id="index-2">
 
 <section class="gardensSection">
-	<div class="header" style="padding: 90px 0;">
+	<div class="header">
 		<h2 class="h1 cursive text-center white">The Gardens</h2>
 	</div>
-	<div class="prose text-center" style="max-width: 670px; margin: 0 auto;">
-		<p class="h4">Find unique patio pieces and design inspiration for you own backyard. We can help you create a private retreat in the comfort of your home.</p>
+	<br>
+	<div class="row">
+		<div class="columns">
+			<div class="prose text-center" style="max-width: 670px; margin: 0 auto;">
+				<p class="h4">Find unique patio pieces and design inspiration for you own backyard. We can help you create a private retreat in the comfort of your home.</p>
+			</div>	
+		</div>
+	</div>
+	<br>
+	<div class="row">
+		
 	</div>
 </section>
 
