64bc5916b7101b9fa1febdfccc04f3e77222a4ac
Nico Watine <nico@studioncreations.com>
2018-05-17 21:21:46 -0500

    Insert correct sizing

--- gardens.php
+++ gardens.php
@@ -21,7 +21,9 @@
 	<div class="header" style="padding: 90px 0;">
 		<h2 class="h1 cursive text-center white">The Gardens</h2>
 	</div>
-	<p class="h4 text-center">Find unique patio pieces and design inspiration for you own backyard. We can help you create a private retreat in the comfort of your home.</p>
+	<div class="prose text-center" style="max-width: 670px; margin: 0 auto;">
+		<p class="h4">Find unique patio pieces and design inspiration for you own backyard. We can help you create a private retreat in the comfort of your home.</p>
+	</div>
 </section>
 
 
