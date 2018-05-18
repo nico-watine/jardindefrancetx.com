5a4770a096834adffa0b94b4a385f9223e321da8
Nico Watine <nico@studioncreations.com>
2018-05-17 21:45:02 -0500

    Add 3rd landscape pic medium

--- gardens.php
+++ gardens.php
@@ -34,13 +34,13 @@
 		<div class="small-10 small-push-1 medium-6 medium-push-0 large-4 columns">
 			<img src="/img/gardens/portrait/view-of-jardin-de-france-garden-pathway.jpg" alt="">
 		</div>
-		<div class="small-10 small-push-1 medium-6 medium-push-0 large-4 columns show-for-medium">
+		<div class="medium-6 large-4 columns show-for-medium">
 			<img src="/img/gardens/portrait/floral-arrangement-with-vintage-water-pitcher.jpg" alt="">
 		</div>
 		<div class="small-10 small-push-1 medium-6 medium-push-0 large-4 columns">
 			<img src="/img/gardens/portrait/jardin-de-france-garden-entry.jpg" alt="">
 		</div>
-		<div class="small-10 small-push-1 medium-6 medium-push-0 large-4 columns end show-for-medium">
+		<div class="medium-6 large-4 columns end show-for-medium">
 			<img src="/img/gardens/portrait/patio-chairs-in-garden.jpg" alt="">
 		</div>
 	</div>
@@ -50,8 +50,11 @@
 			<div class="small-10 small-push-1 medium-4 medium-push-0 large-3 columns">
 				<img src="/img/gardens/landscape/succulents-for-sale-shelf.jpg" alt="">
 			</div>
-			<div class="small-10 small-push-1 medium-4 medium-push-0 large-3 columns end">
+			<div class="small-10 small-push-1 medium-4 medium-push-0 large-3 columns">
 				<img src="/img/gardens/landscape/quaint-sitting-garden.jpg" alt="">
+			</div>
+			<div class="medium-4 large-3 columns end show-for-medium">
+				<img src="/img/gardens/landscape/peaceful-gazebo.jpg" alt="">
 			</div>
 		</div>
 	</div>
