8089a06f13475abf34e486cf36b0351d77b5ce6a
Nico Watine <nico@studioncreations.com>
2019-01-16 18:06:51 -0600

    typo

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
 	<div class="row">
 		<div class="columns">
 			<div class="prose text-center" style="max-width: 670px; margin: 0 auto;">
-				<p class="h4">Find unique patio pieces and design inspiration for you own backyard. We can help you create a private retreat in the comfort of your home.</p>
+				<p class="h4">Find unique patio pieces and design inspiration for your own backyard. We can help you create a private retreat in the comfort of your home.</p>
 			</div>
 		</div>
 	</div>
