b69b561860594a514bc03c999aeb27dc7c9e645f
Nico Watine <nico@studioncreations.com>
2019-01-16 18:06:13 -0600

    Reenable cssnano and run gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var uncss = require('gulp-uncss');
-// var nano = require('gulp-cssnano');
+var nano = require('gulp-cssnano');
 
 gulp.task('default', function () {
     return gulp.src('css/style.css')
         .pipe(uncss({
 	        html: ['http://nicos-mbp.local:5757/']
         }))
-        // .pipe(nano())
+        .pipe(nano())
         .pipe(gulp.dest('./css/opt'));
 });
