6a488bbf0d9a74e1bdaaed024c1f398157725146
Nico Watine <nico@studioncreations.com>
2019-01-16 18:11:56 -0600

    Tweak gulpfile for uncss

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var nano = require('gulp-cssnano');
 gulp.task('default', function () {
     return gulp.src('css/style.css')
         .pipe(uncss({
+        	ignore: ['.js-nav-active .mask'],
 	        html: ['http://nicos-mbp.local:5757/']
         }))
         .pipe(nano())
