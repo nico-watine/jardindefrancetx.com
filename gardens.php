33784810ea408647abe93c930cac89b4734b6e89
Nico Watine <nico@studioncreations.com>
2019-09-07 10:18:16 -0500

    Blank out page content

--- gardens.php
+++ gardens.php
@@ -13,52 +13,6 @@
 </head>
 <body id="index" class="index">
 
-<section class="gardensSection" id="gardens">
-	<div class="header">
-		<h2 class="h1 cursive text-center white">The Gardens</h2>
-	</div>
-	<br>
-	<div class="row">
-		<div class="columns">
-			<div class="prose text-center" style="max-width: 670px; margin: 0 auto;">
-				<p class="h4">Find unique patio pieces and design inspiration for your own backyard. We can help you create a private retreat in the comfort of your home.</p>
-			</div>
-		</div>
-	</div>
-	<br>
-	<div class="row gallery text-center">
-		<div class="small-10 small-push-1 medium-6 medium-push-0 large-3 columns medium-text-right">
-			<img src="/img/gardens/portrait/view-of-jardin-de-france-garden-pathway.jpg" alt="">
-		</div>
-		<div class="medium-6 large-3 columns show-for-medium medium-text-left">
-			<img src="/img/gardens/portrait/floral-arrangement-with-vintage-water-pitcher.jpg" alt="">
-		</div>
-		<div class="small-10 small-push-1 medium-6 medium-push-0 large-3 columns medium-text-right">
-			<img src="/img/gardens/portrait/jardin-de-france-garden-entry.jpg" alt="">
-		</div>
-		<div class="medium-6 large-3 columns end show-for-medium medium-text-left">
-			<img src="/img/gardens/portrait/patio-chairs-in-garden.jpg" alt="">
-		</div>
-	</div>
-	<br>
-	<div class="imageBanner">
-		<div class="row collapse expanded">
-			<div class="small-10 small-push-1 medium-4 medium-push-0 large-3 columns">
-				<img src="/img/gardens/landscape/succulents-for-sale-shelf.jpg" alt="">
-			</div>
-			<div class="small-10 small-push-1 medium-4 medium-push-0 large-3 columns">
-				<img src="/img/gardens/landscape/quaint-sitting-garden.jpg" alt="">
-			</div>
-			<div class="medium-4 large-3 columns end show-for-medium">
-				<img src="/img/gardens/landscape/peaceful-gazebo.jpg" alt="">
-			</div>
-			<div class="large-3 columns end show-for-large">
-				<img src="/img/gardens/landscape/various-succulents-cacti.jpg" alt="">
-			</div>
-		</div>
-	</div>
-</section>
-
 
 </body>
 </html>
